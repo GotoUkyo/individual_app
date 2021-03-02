baef5be969c98576ed6d31d321c72ceaecec95b9
GotoUkyo <itabiyo@yahoo.co.jp>
2021-03-02 13:40:44 +0900

    main_1.html.hamlを作成
    ・難易度☆用のページを作成
    ・トップページの「難易度☆」にリンクを設定

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -3,4 +3,7 @@ class MenusController < ApplicationController
   def index
   end
   
+  def main_1
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'menus#index'
+  get 'main_1', to: 'menus#main_1'
+  #resources :menus
 end
