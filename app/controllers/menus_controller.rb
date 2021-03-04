395e6000a32b896f2024dbdd9252e2cb678adab0
GotoUkyo <itabiyo@yahoo.co.jp>
2021-03-04 15:21:25 +0900

    難易度2及び3用のページを作成

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -6,4 +6,10 @@ class MenusController < ApplicationController
   def main_1
   end
   
+  def main_2
+  end
+
+  def main_3
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'menus#index'
   get 'main_1', to: 'menus#main_1'
+  get 'main_2', to: 'menus#main_2'
+  get 'main_3', to: 'menus#main_3'
   #resources :menus
 end
